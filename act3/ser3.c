b9c77bfa32dbe2f9b89386f5847e485e4e177e6f
NetAcad DEVASC <changeme@example.com>
2023-03-01 22:34:07 +0000

    Code cleaned. NOT TESTED

--- act3/ser3.c
+++ act3/ser3.c
@@ -10,10 +10,9 @@
 
 #include <time.h>
 
+#define MAX_PORT 65535
 
-#define MAX_LINE 100
-
-int compared(int randnum, int num){
+int32_t compared(int randnum, int num){
   if(num>randnum) return 1;
   else if(num<randnum) return -1;
   else return 0;
@@ -31,72 +30,91 @@ int main(int argc, char *argv[]){
         printf("You have to enter 3 arguments, you have entered %d arguments\n", argc-1);
         return 0;
     }
-    //printf("Argc: %d\n", argc);
 
-    int PORT_UDP=atoi(argv[1]); //requestes to file3
-    char *IP=argv[2];           //requestes to file3
-    int PORT_TCP=atoi(argv[3]); //requestes from cli3
+    uint32_t
+        send_value=0,
+        recv_value=0;
 
+    int32_t
+        send_num=-1,
+        recv_num=0,
+        result;
 
-    char send_txt[MAX_LINE];
-    char recv_txt[MAX_LINE];
-    char textin[MAX_LINE];
+    int 
+        port_udp=atoi(argv[1]), //requestes to file3
+        port_tcp=atoi(argv[3]), //requestes from cli3
+        sock_fd,
+        listen_fd,
+        comm_fd,
+        random_number,
+        numlieas,
+        totaliterations=0,
+        randnum=0;
 
-    int listen_fd, comm_fd;
+    char
+        *ip_address=argv[2],          //requestes to file3
+        send_txt[MAX_LINE],
+        recv_txt[MAX_LINE],
+        textin[MAX_LINE];
 
+    struct sockaddr_in servaddr;
+
+    if(port_udp>MAX_PORT || port_udp<=0 || port_tcp>MAX_PORT || port_tcp<=0){
+        printf("that port doesn't exists!\n");
+        return(0);
+    }
+    
     printf("Client side\n");
 
     // Here we create the conection with the server (file3.c), we recieve the number of lines, 
     // we send a random number in that range and we recieve a number from the server
 
-    printf("Connecting to %s:%d\n", IP, PORT_UDP);
+    printf("Connecting to %s:%d\n", ip_address, port_udp);
     
-    int sock_fd;
-    struct sockaddr_in servaddr;
 
     sock_fd=socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd<0){
         perror("socket");
-        return 1;
+        return(1);
     }
 
-    bzero(&servaddr, sizeof(servaddr));
-
     servaddr.sin_family=AF_INET;
-    servaddr.sin_port=htons(PORT_UDP);
+    servaddr.sin_port=htons(port_udp);
 
-    if(inet_pton(AF_INET, IP, &(servaddr.sin_addr))!= 1) {
+    if(inet_pton(AF_INET, ip_address, &(servaddr.sin_addr))!= 1) {
         perror("inet_pton");
-        return 1;
+        return(1);
     }
 
     if (connect(sock_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
         perror("connect");
-        return 1;
+        return(1);
     }
 
-    bzero(recv_txt, MAX_LINE);
-    bzero(send_txt, MAX_LINE);
-
-    if(read(sock_fd, recv_txt, MAX_LINE) < 0){
+    
+    if (read(sock_fd, &recv_value, sizeof(uint32_t)) < 0) {
         perror("read");
-        return 1;
+        return(0);
     }
+    numlieas=ntohl((int32_t)recv_value);
+    printf("\trecieved input: %d\n", numlieas);
 
-    int result = atoi(recv_txt);
-    int random_number = random_number_gen(0, result, 0);
-    sprintf(send_txt, "%d", random_number);
-
-    if (write(sock_fd, send_txt, strlen(send_txt)) < 0) {
+    send_num=(int32_t) random_number_gen(0, numlieas, 0);
+    printf("\n\npicked %d\n", send_num);
+    send_value=htonl((uint32_t)send_num);
+    if (write(sock_fd, &send_value, sizeof(uint32_t)) < 0) {
         perror("write");
-        return 1;
+        return(0);
     }
 
-    printf("Respuesta servidor: %s\n",recv_txt);
+    if (read(sock_fd, &recv_value, sizeof(uint32_t)) < 0) {
+        perror("read");
+        return(0);
+    }
+    random_number=ntohl((int32_t)recv_value);
+    printf("\trecieved input: %d\n", random_number);
 
-    random_number = atoi(recv_txt);
-
-
+    
     printf("Server side\n");
 
     listen_fd=socket(AF_INET, SOCK_STREAM, 0); 
@@ -104,36 +122,49 @@ int main(int argc, char *argv[]){
     bzero(&servaddr, sizeof(servaddr)); 
 
     servaddr.sin_family=AF_INET;
-    servaddr.sin_port=htons(PORT_TCP);
+    servaddr.sin_port=htons(port_tcp);
     servaddr.sin_addr.s_addr=htons(INADDR_ANY);
 
-
-    printf("Waiting for connection on 127.0.0.1:%d\n", PORT_TCP);
+    printf("Waiting for connection on 127.0.0.1:%d\n", port_tcp);
 
     bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
     listen(listen_fd, 1);
 
-    comm_fd=accept(listen_fd, NULL, NULL);
+    while(1){
+        comm_fd=accept(listen_fd, NULL, NULL);
+        
+        send_num=(int32_t) random_number_gen(0, numlieas, 0);
+        printf("\n\npicked %d\n", send_num);
+        send_value=htonl((uint32_t)send_num);
+        if (write(sock_fd, &send_value, sizeof(uint32_t)) < 0) {
+            perror("write");
+            return(0);
+        }
+
+        if (read(sock_fd, &recv_value, sizeof(uint32_t)) < 0) {
+            perror("read");
+            return(0);
+        }
+        random_number=ntohl((int32_t)recv_value);
+        printf("\trecieved input: %d\n", random_number);
+        
+        do{
+            read(comm_fd, &recv_value, sizeof(uint32_t));
+            recv_num=(int32_t) ntohl(recv_value);
+            
+            printf("\t\trecieved input: %d\n", recv_num);
+
+            result=compared(randnum, recv_num);
+            send_value=htonl((uint32_t) result);
+
+            printf("\t\tsending: %d\n\n", result);
+            write(comm_fd, &result, sizeof(uint32_t));
+
+            totaliterations++;
+        }while(result!='0');
     
-    int totaliterations=0, min=0, num, randnum;
-
-    do{
-        bzero(textin,MAX_LINE);
-        read(comm_fd,textin,MAX_LINE);
-        num = atoi(textin);
-        //printf("Text input: %d\n", num);
-
-        result = compared(randnum, num);
-        char textout[5];
-        sprintf(textout, "%d", result);
-        int chrs=strlen(textout);
-        //printf("Text output: %s\n", textout);
-        //printf("\n\n");
-
-        write(comm_fd, textout, chrs);
-        totaliterations++;
-    }while(result!='0');
-    printf("Total iterations: %d\n", totaliterations);
-    return(0);
+        printf("Total iterations: %d\n", totaliterations);
+    }
+    close(listen_fd);
 }
