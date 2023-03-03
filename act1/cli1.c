aab40fbfdbdcd88c50eeb9cbe2d54499c9eed704
PolRubio <apocrb23@gmail.com>
2023-03-03 11:52:43 +0100

    logic game function fix

--- act1/cli1.c
+++ act1/cli1.c
@@ -45,7 +45,7 @@ void setup_tcp_connection(int *sock_fd, struct sockaddr_in *servaddr, char* ip_a
     }
 }
 
-void process_guess(int *current_guess, int *min, int *max){
+void process_guess(int feedback, int *current_guess, int *min, int *max){
     /**
      * PROCESSING THE GUESS NUMBER
      * IF THE DIFFERENCE BETWEEN THE MAXIMUM AND THE MINIMUM IS EQUAL TO 1, THEN THE RANDOM NUMBER IS FOUND
@@ -56,10 +56,10 @@ void process_guess(int *current_guess, int *min, int *max){
     if(*max-*min==1){
         printf("reached the limit!\n");
         *current_guess=*max;
-    }else if(*current_guess==0){
+    }else if(feedback==0){
         printf("\nrandom number is equal to %d\n", *current_guess);
     }else{
-        if(*current_guess>0){
+        if(feedback>0){
             printf("\trandom number is less than %d\n", *current_guess);
             *max=*current_guess;
         }else{
