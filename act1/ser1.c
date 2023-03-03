fb406d903748c5f5a348dad2667e152863050fd5
PolRubio <apocrb23@gmail.com>
2023-03-03 16:54:31 +0100

    COMMENTED

--- act1/ser1.c
+++ act1/ser1.c
@@ -17,11 +17,17 @@
 #define MINIMUM 0
 
 int32_t compared(int32_t randnum, int32_t num){
+    /**
+     * COMPARES THE RANDOM NUMBER WITH THE NUMBER RECEIVED FROM THE CLIENT
+     **/
     int32_t result=num-randnum;
     return (result<0)?-1:(result>0)?1:0;
 }
 
 int random_number_gen(int min_range, int max_range, int seed){
+    /**
+     * GENERATES A PSEUDO RANDOM NUMBER USING THE CURRENT TIME
+     **/
     time_t t;
     long long int current_time = time(&t);
     return (current_time*(seed+2451*732)) % (max_range+1-min_range) + min_range;
@@ -62,13 +68,15 @@ int main(int argc, char *argv[]){
 
     printf("Server side\n");
 
+    // CREATE THE SOCKET AND HANDLE ERRORS
     sock_fd=socket(AF_INET, SOCK_STREAM, 0); 
     if(sock_fd<0){
         fprintf(stderr,"Failed to create socket: %s\n", strerror(sock_fd));
         return 0;
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    // BIND THE SOCKET TO THE PORT AND HANDLE ERRORS
+    memset(&servaddr, 0, sizeof(servaddr)); 
     servaddr.sin_family=AF_INET;
     servaddr.sin_port=htons(port);
     servaddr.sin_addr.s_addr=htons(INADDR_ANY);
@@ -78,6 +86,7 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
+    // LISTEN FOR CONNECTIONS AND HANDLE ERRORS
     if(listen(sock_fd, 1)<0){
         perrro("listen");
         return 0;
@@ -85,28 +94,38 @@ int main(int argc, char *argv[]){
     
     while(1){
         printf("Waiting for connection on port %d\n" , port);
+
+        // ACCEPT CONNECTIONS AND HANDLE ERRORS
         comm_fd=accept(sock_fd, NULL, NULL);
         if(comm_fd<0){
             perror("accept connection");
             return 0;
         }
 
+        // GENERATE A RANDOM NUMBER
         randnum=random_number_gen(min, max, totaliterations);
         printf("\nrandom number: %d\n\n", randnum);
         
         do{
+            // READ THE INPUT FROM THE CLIENT AND HANDLE ERRORS
             if(read(comm_fd, &recv_value, sizeof(uint32_t))<0){
                 perror("read");
                 return 0;
             }
+
+            // CONVERT FROM NETWORK BYTE ORDER TO HOST BYTE ORDER
             recv_num=(int32_t) ntohl(recv_value);
 
             printf("\trecieved input: %d\n", recv_num);
 
             result=compared(randnum, recv_num);
+
+            // CONVERT FROM HOST BYTE ORDER TO NETWORK BYTE ORDER
             send_value=htonl((uint32_t) result);
 
             printf("\tsending: %d\n\n", result);
+
+            // SEND THE RESULT TO THE CLIENT AND HANDLE ERRORS
             if(write(comm_fd, &result, sizeof(uint32_t))<0){
                 perror("wite");
                 return 0;
@@ -117,5 +136,6 @@ int main(int argc, char *argv[]){
         
         printf("Total iterations: %d\n", totaliterations);
     }
+    // CLOSE THE SOCKET
     close(sock_fd);
 }
